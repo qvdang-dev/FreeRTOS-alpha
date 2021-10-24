3c534b3c9f66fd307a53b3eae733833e6691b15a
qvdang <qvdang.96@gmail.com>
2021-10-24 22:02:41 +0700

    create new file for task handler

---
+++ Core/Src/task_handler.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/* USER CODE BEGIN 4 */
+static void task_led_handler(void* parameter)
+{
+  for(;;)
+  {
+
+  }
+}
+
+static void task_menu_handler(void* parameter)
+{
+  for(;;)
+  {
+    
+  }
+}
+
+static void task_rtc_handler(void* parameter)
+{
+  for(;;)
+  {
+    
+  }
+}
+
+static void task_print_handler(void* parameter)
+{
+  for(;;)
+  {
+    
+  }
+}
+
+static void task_command_handler(void* parameter)
+{
+  for(;;)
+  {
+    
+  }
+}
